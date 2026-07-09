49aaa0059e79f02475d2c4ec46554dce5fa37dc5
gadiel <gadielamilec@gmail.com>
2026-07-09 19:19:07 -0400

    Rebalance homepage hero on tablet

--- components/header.php
+++ components/header.php
@@ -339,7 +339,7 @@ if ($bodyVariant === 'collection-light') {
     $rdBodyStyle = '--disp:' . $rdFont['stack'] . ';--rd-heading-weight:' . (int) $rdFont['weight'];
     ?>
     <link href="<?= h(redesignFontsUrl($rdDesign['font'], $rdEditorMode)) ?>" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/redesign.css?v=57">
+    <link rel="stylesheet" href="/assets/css/redesign.css?v=58">
     <?php endif; ?>
 
     <!-- Deferred scripts (non-blocking) -->
