76467b3abe2da4db928b59ccdefd251b654bfd15
gadiel <gadielamilec@gmail.com>
2026-07-10 05:59:31 -0400

    Bump redesign stylesheet cache key

--- components/header.php
+++ components/header.php
@@ -339,7 +339,7 @@ if ($bodyVariant === 'collection-light') {
     $rdBodyStyle = '--disp:' . $rdFont['stack'] . ';--rd-heading-weight:' . (int) $rdFont['weight'];
     ?>
     <link href="<?= h(redesignFontsUrl($rdDesign['font'], $rdEditorMode)) ?>" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/redesign.css?v=60">
+    <link rel="stylesheet" href="/assets/css/redesign.css?v=61">
     <?php endif; ?>
 
     <!-- Deferred scripts (non-blocking) -->
